f5b69d73374cea078faab7c3bb38f1ef84d7061f
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-12-05 16:55:12 -0300

    reparar areas de transición

--- prueba-noviembre/Escenas/transition_2.gd
+++ prueba-noviembre/Escenas/transition_2.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		Global.runas = 0
--- prueba-noviembre/Escenas/transition_area.gd
+++ prueba-noviembre/Escenas/transition_area.gd
@@ -3,6 +3,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		Global.runas = 0
+		#Global.runas = 0
 		get_tree().call_deferred("change_scene_to_file","res://Escenas/escena_2.tscn")
 		
