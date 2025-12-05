9017f73ed0118d1a697a6361ba432ff939aad8f1
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-12-05 16:50:53 -0300

    ordenar proyecto

--- prueba-noviembre/Escenas/menu_principal.gd
+++ prueba-noviembre/Escenas/menu_principal.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _on_iniciar_pressed() -> void:
-	get_tree().call_deferred("change_scene_to_file","res://Escenas/nueva_escena_principal.tscn")
+	get_tree().call_deferred("change_scene_to_file","res://Escenas/escena_principal.tscn")
 	
--- prueba-noviembre/Escenas/transition_2.gd
+++ prueba-noviembre/Escenas/transition_2.gd
@@ -4,5 +4,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		Global.runas = 0
-		get_tree().call_deferred("change_scene_to_file","res://Escenas/escena_2.tscn")
-		
+		get_tree().call_deferred("change_scene_to_file","res://Escenas/escena_principal.tscn")
