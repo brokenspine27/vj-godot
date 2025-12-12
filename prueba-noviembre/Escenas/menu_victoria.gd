d9c1be423a99b5d82e28518851220e9af80d7b74
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-12-12 14:51:14 -0300

    menu victoria

---
+++ prueba-noviembre/Escenas/menu_victoria.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+func _on_iniciar_pressed() -> void:
+	Global.vidas = 6
+	Global.runas = 0
+	get_tree().call_deferred("change_scene_to_file","res://Escenas/menu_principal.tscn" )
