f03401daa55cabdf23b73d3355ed3ab10030ea52
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-12-12 10:32:36 -0300

    agregar quitar vidas

--- prueba-noviembre/Escenas/hud_runas.gd
+++ prueba-noviembre/Escenas/hud_runas.gd
@@ -1,14 +1,13 @@
 extends CanvasLayer
 
-@onready var numero: Label = $numero_runas
+@onready var numero_runas: Label = $numero_runas
+@onready var numero_vidas: Label = $numero_vidas
 
 func _process(_delta):
+	numero_runas.text = str(Global.runas)
 	
 	var vidas = ""
-	numero.text = str(Global.runas)
-	
 	for i in range(Global.vidas):
 		vidas += "❤️"
 		
-	$numero_vidas.text = vidas
-	
+	numero_vidas.text = vidas
